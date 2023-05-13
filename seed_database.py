1d0bd381f93f39e6b71128a2af39293535df9d78
Faycal <faycalbourouina@gmail.com>
2023-05-12 23:28:05 -0500

    mock favorite recipes data added to recipes and favorites  tables

--- seed_database.py
+++ seed_database.py
@@ -28,3 +28,29 @@ for user in mock_db_data['users']:
 
 model.db.session.add_all(users_in_db)
 model.db.session.commit()
+
+
+for range in range(10):
+    # Select a random user form the list of users in the db
+    user = choice(users_in_db)
+    # Select a random recipe from the mock data
+    recipe = choice(mock_db_data['recipes'])
+    
+    # Add recipe to the db
+    liked_recipe = crud.add_recipe(recipe)
+    recipes_in_db.append(liked_recipe)
+
+    # Add liked recipe to user's favorites
+    new_favorite = crud.add_favorite(user, liked_recipe)
+    if new_favorite:
+        favorites_in_db.append(new_favorite)
+
+print("users_in_db", users_in_db)
+print("recipes_in_db", recipes_in_db)
+print("favorites_in_db", favorites_in_db)
+
+model.db.session.add_all(recipes_in_db)
+model.db.session.commit()
+
+model.db.session.add_all(favorites_in_db)
+model.db.session.commit()
