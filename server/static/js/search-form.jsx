8f847bcd4f7943ef858cd1c3c9c17db063c9996d
Faycal <faycalbourouina@gmail.com>
2023-05-17 13:54:55 -0500

    search-form: component created

--- server/static/js/search-form.jsx
+++ server/static/js/search-form.jsx
@@ -1,11 +1,149 @@
+const { useState } = React;
 
-function SearchForm() {
+function SearchForm({ onSearch }) {
+  const [query, setQuery] = useState('');
+  const [diet, setDiet] = useState('');
+  const [cuisine, setCuisine] = useState('');
+  const [intolerances, setIntolerances] = useState([]);
+  const [includeIngredients, setIncludeIngredients] = useState([]);
+  const [type, setType] = useState([]);
+  const [maxReadyTime, setMaxReadyTime] = useState('');
+
+  const diets = ['Vegetarian', 'Vegan', 'Ketogenic', 'Paleo'];
+  const cuisines = ['Italian', 'Chinese', 'Indian', 'Mexican'];
+  const intoleranceOptions = ['Dairy', 'Egg', 'Gluten', 'Peanut'];
+  const typeOptions = ['Main Course', 'Side Dish', 'Dessert', 'Appetizer'];
+
+  const handleIntoleranceChange = event => {
+    const { value } = event.target;
+    setIntolerances(intolerances =>
+      intolerances.includes(value)
+        ? intolerances.filter(i => i !== value)
+        : [...intolerances, value]
+    );
+  };
+
+  const handleIncludeIngredientsChange = event => {
+    setIncludeIngredients(event.target.value.split(','));
+  };
+
+  const handleTypeChange = event => {
+    const { value } = event.target;
+    setType(type =>
+      type.includes(value) ? type.filter(t => t !== value) : [...type, value]
+    );
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    onSearch({
+      query,
+      diet,
+      cuisine,
+      intolerances,
+      includeIngredients,
+      type,
+      maxReadyTime,
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         Query:
-        <input type="text" name="query" />
+        <input
+          type="text"
+          name="query"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
       </label>
+      <br />
+      <label>
+        Diet:
+        <select
+          name="diet"
+          value={diet}
+          onChange={event => setDiet(event.target.value)}
+        >
+          <option value="">--Select a diet--</option>
+          {diets.map(diet => (
+            <option key={diet} value={diet}>
+              {diet}
+            </option>
+          ))}
+        </select>
+      </label>
+      <br />
+      <label>
+        Cuisine:
+        <select
+          name="cuisine"
+          value={cuisine}
+          onChange={event => setCuisine(event.target.value)}
+        >
+          <option value="">--Select a cuisine--</option>
+          {cuisines.map(cuisine => (
+            <option key={cuisine} value={cuisine}>
+              {cuisine}
+            </option>
+          ))}
+        </select>
+      </label>
+      <br />
+      <fieldset>
+        <legend>Intolerances:</legend>
+        {intoleranceOptions.map(intolerance => (
+          <label key={intolerance}>
+            <input
+              type="checkbox"
+              name="intolerances"
+              value={intolerance}
+              checked={intolerances.includes(intolerance)}
+              onChange={handleIntoleranceChange}
+            />
+            {intolerance}
+          </label>
+        ))}
+      </fieldset>
+      <br />
+      <label>
+        Include Ingredients:
+        <input
+          type="text"
+          name="includeIngredients"
+          onChange={handleIncludeIngredientsChange}
+        />
+      </label>
+      <br />
+      <fieldset>
+        <legend>Type:</legend>
+        {typeOptions.map(typeOption => (
+          <label key={typeOption}>
+            <input
+              type="checkbox"
+              name="type"
+              value={typeOption}
+              checked={type.includes(typeOption)}
+              onChange={handleTypeChange}
+            />
+            {typeOption}
+          </label>
+        ))}
+      </fieldset>
+      <br />
+      <label>
+        Max Ready Time:
+        <input
+          type="number"
+          name="maxReadyTime"
+          value={maxReadyTime}
+          onChange={event => setMaxReadyTime(event.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Search</button>
     </form>
   );
 }
