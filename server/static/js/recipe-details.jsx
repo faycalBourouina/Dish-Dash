e33e41bbb76dce84574cf2030c0c4cdd31f4f60d
Faycal <faycalbourouina@gmail.com>
2023-05-18 12:04:06 -0500

    RecipeDetails: Component implemented

--- server/static/js/recipe-details.jsx
+++ server/static/js/recipe-details.jsx
@@ -1,7 +1,16 @@
 function RecipeDetails({ recipe }) {
-    return (
-      <div>
-        recipe details
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>{recipe.title}</h2>
+      <img src={recipe.image} alt={recipe.title} />
+      <h3>Ingredients:</h3>
+      <ul>
+        {recipe.ingredients.map((ingredient) => (
+          <li key={ingredient.id}>{ingredient.original_name}</li>
+        ))}
+      </ul>
+      <h3>Instructions:</h3>
+      <div dangerouslySetInnerHTML={{ __html: recipe.instructions }}></div>
+    </div>
+  );
+}
