55c29f14028f182df16c8339f96871a3367f72be
Faycal <faycalbourouina@gmail.com>
2023-05-19 20:02:07 -0500

    handleUpdateFavorite: moved up to Layout component

--- server/static/js/layout.jsx
+++ server/static/js/layout.jsx
@@ -13,6 +13,44 @@ function Layout({ isLogged }) {
         setSelectedRecipe(null); // clear the selected recipe
         setRecipes(results);
     }
+
+    async function handleUpdateFavorites() {
+      const userId = isLogged;
+      const recipeId = selectedRecipe.id;
+      const response = await fetch(`/users/${userId}/favorites/${recipeId}`, {
+        method: "PATCH",
+      });
+      const data = await response.json();
+    
+      if (response.status === 409 && data.message === "Favorite already exists") {
+        // Send a DELETE request to remove the favorite
+        const deleteResponse = await fetch(`/users/${userId}/favorites/${recipeId}`, {
+          method: "DELETE",
+        });
+        if (deleteResponse.ok) {
+          // Favorite removed successfully
+          console.log("Favorite removed");
+          // Update the recipes state by filtering out the deleted recipe
+          const updatedRecipes = recipes.filter((r) => r.id !== recipeId);
+          setRecipes(updatedRecipes);
+          setSelectedRecipe("");
+          setActiveTab("favorites")
+          console.log("activeTab after delete", activeTab)
+        } else {
+          // Error handling
+          console.error("Failed to remove favorite");
+        }
+      } else if (response.ok) {
+        // Recipe added to favorites successfully
+        console.log("Recipe added to favorites");
+        const { favorite } = data;
+        // Update the recipes state by adding the new favorite recipe
+        setRecipes([...recipes, favorite]);
+      } else {
+        // Error handling for other response statuses
+        console.error("Failed to add recipe to favorites");
+      }
+    }
     
     async function fetchLandingRecipes() {
         const response = await fetch("/");
@@ -52,9 +90,15 @@ function Layout({ isLogged }) {
           <SearchForm onSearch={handleSearch} />
           <div>
             {selectedRecipe ? (
-              <RecipeDetails recipe={selectedRecipe} />
+              <RecipeDetails 
+                recipe={selectedRecipe}
+                handleUpdateFavorites={handleUpdateFavorites}
+                />
             ) : (
-              <RecipeList recipes={recipes} onRecipeClick={handleRecipeClick} />
+              <RecipeList 
+                recipes={recipes} 
+                onRecipeClick={handleRecipeClick} 
+              />
             )}
           </div>
         </div>
--- server/static/js/recipe-details.jsx
+++ server/static/js/recipe-details.jsx
@@ -1,37 +1,5 @@
-function RecipeDetails({ recipe }) {
+function RecipeDetails({ recipe, handleUpdateFavorites }) {
   
-  async function handleUpdateFavorites() {
-    const userId = isLogged;
-    const recipeId = recipe.id;
-    const response = await fetch(`/users/${userId}/favorites/${recipeId}`, {
-      method: "PATCH",
-    });
-    const data = await response.json();
-
-    if (response.status === 409 && data.message === "Favorite already exists") {
-      // Send a DELETE request to remove the favorite
-      const deleteResponse = await fetch(`/users/${userId}/favorites/${recipeId}`, {
-        method: "DELETE",
-      });
-      if (deleteResponse.ok) {
-        // Favorite removed successfully
-        console.log("Favorite removed");
-        // Handle the updated favorite state if needed
-      } else {
-        // Error handling
-        console.error("Failed to remove favorite");
-      }
-    } else if (response.ok) {
-      // Recipe added to favorites successfully
-      console.log("Recipe added to favorites");
-      const { favorite } = data;
-      // Handle the updated favorite state if needed
-    } else {
-      // Error handling for other response statuses
-      console.error("Failed to add recipe to favorites");
-    }
-  }
-
   return (
     <div>
       <h2>{recipe.title}</h2>
