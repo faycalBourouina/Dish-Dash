b98ea08ff10896fa93ea965a271490ac5c7f0a1a
Faycal Bourouina <faycalbourouina@gmail.com>
2024-07-09 11:32:59 -0500

    Creducers ir moved to Next.js app and syntax updated to es6 module syntax

---
+++ client/app/reducers/FavoritesReducer.js
@@ -0,0 +1,24 @@
+import { ADD_RECIPE, REMOVE_RECIPE, FETCH_FAVORITES } from './actionTypes';
+
+const favoritesReducer = (state, action) => {
+  switch (action.type) {
+      case ADD_RECIPE:
+          // Add the new recipe to the favoriteRecipes array
+          return { ...state, favoriteRecipes: [...state.favoriteRecipes, action.payload.favorite] };
+
+      case REMOVE_RECIPE:
+          // Remove the recipe with the specified ID from favoriteRecipes
+          return {
+              ...state,
+              favoriteRecipes: state.favoriteRecipes.filter((recipe) => recipe.id !== action.payload.removedRecipeId),
+          };
+
+      case FETCH_FAVORITES:
+          // Replace entire favoriteRecipes array with new data from action payload
+          return { ...state, favoriteRecipes: action.payload.favorites };
+
+      default:
+          // If action type doesn't match any defined cases, return current state
+          return state;
+  }
+};
---
+++ client/app/reducers/SelectedReducer.js
@@ -0,0 +1,21 @@
+import { ADD_RECIPE, REMOVE_RECIPE, UPDATE_SELECTED } from './actionTypes';
+
+const selectedReducer = (state, action) => {
+    switch (action.type) {
+        case ADD_RECIPE:
+            // Mark the selected recipe as a favorite
+            return { ...state, selectedRecipe: { ...state.selectedRecipe, isFavorite: true } };
+
+        case REMOVE_RECIPE:
+            // Remove the selected recipe from favorites
+            return { ...state, selectedRecipe: { ...state.selectedRecipe, isFavorite: false } };
+
+        case UPDATE_SELECTED:
+            // Update the entire selected recipe with new data
+            return { ...state, selectedRecipe: action.payload.selected };
+
+        default:
+            // If action type doesn't match any defined cases, return current state
+            return state;
+    }
+};
