a20cb58bb72d9a4afcc1a45a1bf9fdf7f3a7f183
Faycal Bourouina <faycalbourouina@gmail.com>
2024-07-09 12:16:29 -0500

    Added a barrel file to centralize export/import components

---
+++ client/app/components/index.js
@@ -0,0 +1,27 @@
+// components/index.js
+
+export { default as AuthForm } from './auth-form'; // Adjust filenames as needed
+export { default as AuthModal } from './auth-modal';
+export { default as DemoMessage } from './demo-message';
+export { default as FavoriteMessageBar } from './favorite-message-bar';
+export { default as Footer } from './footer';
+export { default as ItemDetails } from './item-details';
+export { default as ItemListSkeleton } from './item-list-skeleton';
+export { default as ItemList } from './item-list';
+export { default as NavBar } from './navbar';
+export { default as RecipeDetails } from './recipe-details';
+export { default as RecipeItem } from './recipe-item';
+export { default as RecipeListSkeleton } from './recipe-list-skeleton';
+export { default as RecipeList } from './recipe-list';
+export { default as RecipeTags } from './recipe-tags';
+export { default as SearchForm } from './search-form';
+export { default as SimilarRecipes } from './similar-recipes';
+export { default as TagsModal } from './tags-modal';
+
+
+
+
+
+
+
+
