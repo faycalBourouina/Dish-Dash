14f8545ba205c9851b6f2dedbe21187604b43b12
Faycal Bourouina <faycalbourouina@gmail.com>
2024-07-09 12:29:47 -0500

    Added a barrel file to centralize export/import reducers

---
+++ client/app/reducers/index.js
@@ -0,0 +1,6 @@
+export { default as actionTypes } from './action-types';
+export { default as landingReducer } from './LandingReducer';
+export { default as selectedReducer } from './SelectedReducer';
+export { default as FavoritesReducer } from './favorites-reducer';
+export { default as landingReducer } from './LandingReducer';
+export { default as selectedReducer } from './SelectedReducer';
